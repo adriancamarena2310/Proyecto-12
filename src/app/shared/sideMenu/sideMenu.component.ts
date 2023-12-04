e16754bb9f4266c0e3eb32e3de83a9dd8cb4d367
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 19:34:01 -0600

    video-390 Creación de archivos y directorios

---
+++ src/app/shared/sideMenu/sideMenu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sideMenu',
+  templateUrl: './sideMenu.component.html',
+  styleUrls: ['./sideMenu.component.css']
+})
+export class SideMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/title/title.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  standalone:true,
+  imports:[CommonModule],
+  templateUrl: './title.component.html',
+ styles:``
+})
+export class TitleComponent  {
+
+
+}
