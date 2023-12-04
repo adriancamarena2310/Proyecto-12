c6a0cea7009d94996db824679b0964bd4af3ca42
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 21:35:32 -0600

    video-402 Defer Triggers - Interacciones

--- src/app/shared/heavy-loaders/heavy-loaders-slow-fast.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow-fast.component.ts
@@ -1,15 +1,23 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone:true,
   imports:[CommonModule],
   template: `
-<h1>Hola mundo</h1>
+  <section [ngClass]="['w-full', cssClass ]">
+
+<ng-content />
+
+</section>
   `
 })
 export class HeavyLoadersFastComponent  {
 
+  @Input({ required: true }) cssClass!: string;
 
+  constructor() {
+    console.log('HeavyLoader Fast creado');
+  }
 }
