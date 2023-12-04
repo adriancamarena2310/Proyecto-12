2640641e0c1ecf8298ac8a7ecbe3db2d6b825827
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 19:51:17 -0600

    video-392 Configuración del dashboard - Tailwind

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SideMenuComponent } from '../shared/sideMenu/sideMenu.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,SideMenuComponent],
   templateUrl: './dashboard.component.html',
   styles: ``
 })
--- src/app/shared/sideMenu/sideMenu.component.ts
+++ src/app/shared/sideMenu/sideMenu.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sideMenu',
+  standalone: true,
   templateUrl: './sideMenu.component.html',
   styleUrls: ['./sideMenu.component.css']
 })
 export class SideMenuComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // const dashboardRoutes = routes
+    //   .map( route => route.children ?? [] )
+    //   .flat()
+    //   .filter( route => route && route.path )
+    //   .filter( route => !route.path?.includes(':') )
+    // console.log(dashboardRoutes);
+  }
 
   ngOnInit() {
   }
+
+  public menuItems = routes
+    .map((route) => route.children ?? [])
+    .flat()
+    .filter((route) => route && route.path)
+    .filter((route) => !route.path?.includes(':'));
 
 }
