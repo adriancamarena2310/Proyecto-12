f1831b7403ba01d5af67bb0382ac81c96c493e46
Adrian Camarena <adriancamarena2310mail.com>
2023-12-03 20:02:28 -0600

    video-394 Configurar alias de importaciones en TypeScript

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { SideMenuComponent } from '../shared/sideMenu/sideMenu.component';
+import { SideMenuComponent } from '@shared/sideMenu/sideMenu.component';
 
 @Component({
   standalone: true,
